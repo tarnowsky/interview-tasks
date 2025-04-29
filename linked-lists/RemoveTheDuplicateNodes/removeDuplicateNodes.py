0d1cbf0320e1710c297b50d97f68829580aaff8a
Michał Tarnowski <michal.tarnowski03@gmail.com>
2025-04-29 18:43:34 +0200

    Refactor linked list classes to implement removeDuplicateNodes method and update class structure

--- linked-lists/RemoveTheDuplicateNodes/removeDuplicateNodes.py
+++ linked-lists/RemoveTheDuplicateNodes/removeDuplicateNodes.py
@@ -1,22 +1,25 @@
 from __future__ import annotations
+from collections import defaultdict
 from lib.LinkedList import LinkedList as LL
 
-class ReverseExtension(LL):
+class LinkedListDuplicatesExtension(LL):
     def __init__(self, data=0):
         super().__init__(data)
 
-    def reverse(self) -> ReverseExtension:
-        prev = None
-        curr = self
-        while curr:
-            next_node = curr._next
-            curr._next = prev
-            prev = curr
-            curr = next_node
-        return curr
+    def removeDuplicateNodes(self) -> LinkedListDuplicatesExtension:
+        head = self
+        nodes = defaultdict(bool)
+        nodes[head._data] = True
+        while head._next:
+            if nodes[head._next._data]:
+                head._next = head._next._next
+            else:
+                nodes[head._next._data] = True
+                head = head._next
+        return self
     
 
-head = ReverseExtension()
+head = LinkedListDuplicatesExtension().insert(0, 1, 1, 2, 3, 4, 5, 6, 6)
 print(head)
-head = head.reverse()
+head = head.removeDuplicateNodes()
 print(head)
--- linked-lists/ReverseList/reverseList.py
+++ linked-lists/ReverseList/reverseList.py
@@ -1,41 +1,11 @@
 from __future__ import annotations
-from collections import defaultdict
-
-class Node:
+from ..lib.LinkedList import LinkedList as LL
+class Node(LL):
     def __init__(self, data=0):
-        self._next: Node = None
-        self._data = data
-
-    def insert(self, data, *args) -> Node:
-        new_node = Node(data)
-        head = self
-        while head._next: head = head._next
-        head._next = new_node
-        for next_data in args:
-            head = head._next
-            head._next = Node(next_data)
-        return self
-
+        super().__init__(data)
+        
     def reverse(self) -> Node:
-        head = self
-        if not head._next:
-            return self
-        prev = None
-        curr = head
-        while curr:
-            next_node = curr._next
-            curr._next = prev
-            prev = curr
-            curr = next_node
-        return prev
-
-    def __str__(self):
-        result = ''
-        head = self
-        while head:
-            result += f'{head._data} -> '
-            head = head._next
-        return result[:-4]
+        pass
 
 head = Node().insert(1).insert(2).insert(5)
 print(head)
